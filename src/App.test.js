681b4b708479727aed9a42f962de179bdfb06d4b
Joao Neto <joao.gsneto@gmail.com>
2020-06-17 18:51:19 -0300

    feat: test sum

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -8,8 +8,17 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('raise an error while test', () => {
-  const { queryByText } = render(<App />);
-  const linkElement = queryByText(/test/i);
-  expect(linkElement).toBeNull();
+test('test if 1 + 1 is 2', async () => {
+  const { getByText, findByPlaceholderText, getByTestId } = render(<App />);
+  const input1 = await findByPlaceholderText( 'op1');
+  const input2 = await findByPlaceholderText( 'op2');
+
+  fireEvent.change( input1, { target: { value: 1 } } );
+  fireEvent.change( input2, { target: { value: 1 } } );
+
+  fireEvent.click( getByText('Somar') );
+
+  const somaResult = getByTestId('somaResult');
+
+  expect(somaResult.textContent).toBe('2')
 });
---
+++ src/Soma.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+
+function Soma() {
+  const [ op1, setOp1 ] = useState('');
+  const [ op2, setOp2 ] = useState('');
+
+  const [ res, setRes ] = useState('');
+
+
+  const somar = function() {
+      setRes( Number(op1) + Number(op2) );
+  }
+
+  return (
+    <div className="Soma">
+      <div>
+          <input name="op1" placeholder="op1" onChange={event => setOp1(event.target.value)} />
+
+          <input name="op2" placeholder="op2" onChange={event => setOp2(event.target.value)} />
+
+          <button id="click" onClick={somar}>Somar</button>
+      </div>
+
+      <div data-testid="somaResult" className="result">
+        {res}
+      </div>
+    </div>
+  );
+}
+
+export default Soma;
